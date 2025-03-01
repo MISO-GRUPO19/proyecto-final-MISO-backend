89e375e0bece9866365a5e5771c1ef4c3cfec65a
Carlos Tiguaque <c.tiguaque@uniandes.edu.co>
2025-03-01 09:27:28 -0500

    Se incluye monitoreo con uptrace

--- products-management/src/main.py
+++ products-management/src/main.py
@@ -4,10 +4,17 @@ from .api.products import products
 from .errors.errors import ApiError
 import os
 from .models.database import init_db
+import uptrace
+
+# Inicializar Uptrace
+uptrace.configure_opentelemetry(
+    dsn="https://t0Yfk4-x00bJJt5WlUqreg@api.uptrace.dev/5872",
+    service_name="products-management",
+    service_version="1.0.0",
+)
 
 load_dotenv('./.env.development')
 APP_PORT = int(os.getenv("APP_PORT", default=5000))
-
 
 app = Flask(__name__)
 app.register_blueprint(products)
