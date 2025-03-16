289a64d1568493a20e43dedf8cbd693eb8454f94
Carlos Tiguaque <c.tiguaque@uniandes.edu.co>
2025-03-15 20:33:40 -0500

    Se adiciona versión base de users

---
+++ authentications-management/src/errors/errors.py
@@ -0,0 +1,11 @@
+class ApiError(Exception):
+    code = 422
+    description = "Default message"
+
+class InvalidData(ApiError):
+    code = 400
+    description = "Invalid data"
+    
+class PasswordMismatch(ApiError):
+    code = 400
+    description = "Password mismatch"
