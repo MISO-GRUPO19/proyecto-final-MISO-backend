5e436cce446ab5a4491e368ae194fdb78339b0f4
Carlos Tiguaque <c.tiguaque@uniandes.edu.co>
2025-03-22 10:47:03 -0500

    Se ajustan pruebas

--- authentications_management/tests/test_database.py
+++ authentications_management/tests/test_database.py
@@ -7,11 +7,3 @@ def test_init_db(monkeypatch):
     monkeypatch.setattr(base.metadata, 'create_all', mock.Mock())
     init_db()
     assert True
-
-def test_db_session():
-    user = Users(email='test@example.com', password='password123', role=Role.Cliente)
-    db_session.add(user)
-    db_session.commit()
-    retrieved_user = db_session.query(Users).filter_by(email='test@example.com').first()
-    assert retrieved_user is not None
-    assert retrieved_user.email == 'test@example.com'
