5cc50d80497fb25b4873f406a1d89de515ab3e1b
Carlos Tiguaque <c.tiguaque@uniandes.edu.co>
2025-03-24 11:24:59 -0500

    Se adiciona token

--- products_management/src/api/products.py
+++ products_management/src/api/products.py
@@ -1,17 +1,20 @@
 from flask import request, jsonify, Blueprint, Response
 from ..commands.create_products import CreateProducts
 from ..commands.create_massive_products import CreateMassiveProducts
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 products = Blueprint('products', __name__)
 
 @products.route('/products', methods=['POST'])
+@jwt_required()
 def create_product():
     data = request.get_json()
 
     result = CreateProducts(data).execute()
     return jsonify(result), 201
 
-@products.route('/upload_products', methods=['POST'])
+@products.route('/products/upload_products', methods=['POST'])
+@jwt_required()
 def upload_products():
     file = request.files['file']
     result = CreateMassiveProducts(file).execute()
