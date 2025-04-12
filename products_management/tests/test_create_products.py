9deeaa8f823cf7eb06edf92ce093c50f82c9a158
Juan Camilo <j.villadap@uniandes.edu.co>
2025-04-12 00:13:03 -0500

    Primera prueba unitaria añadida, verificacion de creacion de bodegas

--- products_management/tests/test_create_products.py
+++ products_management/tests/test_create_products.py
@@ -218,23 +218,30 @@ class TestCreateProducts(unittest.TestCase):
         
         self.assertIn("El peso debe ser mayor que cero.", str(e.exception))
     
-    def test_create_warehouses(self):
-        data = {
-        "name": "Product Name",
-        "description": "Product Description",
-        "price": 19.99,
-        "category": "Condimentos y Especias",
-        "weight": 1.5,
-        "barcode": "1234567890123",
-        "provider_id": "123e4567-e89b-12d3-a456-426614174000",
-        "batch": "Batch001",
-        "best_before": "2025-12-31T23:59:59",
-        "quantity": 100
+    def test_create_warehouses(self, mock_requests_get):
+        token = self.get_jwt_token()
+        headers = {
+            'Authorization': f'Bearer {token}'
         }
-    
-        auth_token = self.get_jwt_token()  
-        command = CreateProducts(data, auth_token)  
-        command.execute()  
+        with self.client:
+            
+            # Simular respuesta HTTP para validar proveedores
+            mock_requests_get.return_value.status_code = 200
+            mock_requests_get.return_value.json.return_value = {"valid": True}
+            
+            response = self.client.post('/products', json={
+                'name': 'Product Name',
+                'description': 'Product Description',
+                'price': 19.99,
+                'category': 'Condimentos y Especias',
+                'weight': 1.5,
+                'barcode': '1234567890123',
+                'provider_id': '123e4567-e89b-12d3-a456-426614174000',
+                'batch': 'Batch001',
+                'best_before': '2025-12-31T23:59:59',
+                'quantity': 100
+            }, headers=headers)
+            print(response.json)  
         with self.app.app_context():
             from products_management.src.models.products import Warehouses
             warehouses = db_session.query(Warehouses).all()  
