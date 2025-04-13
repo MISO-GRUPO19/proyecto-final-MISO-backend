04d7823a1cbdb15c7558dfcb03c6131e457dd1c5
Juan Camilo <j.villadap@uniandes.edu.co>
2025-04-13 13:40:10 -0500

    modificacion de pruebas unitarias

--- products_management/tests/test_get_product_by_id.py
+++ products_management/tests/test_get_product_by_id.py
@@ -6,43 +6,33 @@ from products_management.src.api.products import products
 from products_management.src.models.database import db_session
 from products_management.src.queries.get_product_by_id import GetById
 from products_management.src.errors.errors import InvalidData, NotFound
+from types import SimpleNamespace
 
 class TestGetProductById(unittest.TestCase):
 
-    def setUp(self):
-        self.app = Flask(__name__)
-        self.app.config['JWT_SECRET_KEY'] = 'qwerty'  # Cambia esto por una clave secreta segura
-        self.app.config['TESTING'] = True
+    @patch('products_management.src.queries.get_products_by_id.db_session')
+    def test_execute_returns_correct_product_warehouse(self, mock_db_session):
+        mock_db_session.query().outerjoin().group_by().all.return_value = [
+            SimpleNamespace(
+                name="Producto A", 
+                barcode="123ABC", 
+                stock=10, 
+                price=5.5,
+                warehouse_info={
+                    "name": "Bodega A",
+                    "address": "123 Main St",
+                    "quantity": 100,
+                    "shelf": "A",
+                    "aisle": "1",
+                    "level": 1
+                }
+                )
+        ]
 
-        jwt = JWTManager(self.app)
-        self.app.register_blueprint(products)
-
-        self.client = self.app.test_client()
-
-        with self.app.app_context():
-            db_session.remove()
-    
-    def tearDown(self):
-        with self.app.app_context():
-            db_session.remove()
-
-    def get_jwt_token(self):
-        with self.app.app_context():
-            access_token = create_access_token(identity='test_user')
-            return access_token
-    @patch('requests.post')
-    def test_get_product_by_barcode(self, mock_requests_post):
-        token = self.get_jwt_token()
-        headers = {
-            'Authorization': f'Bearer {token}'
-        }
-        with self.client:
-            mock_requests_post.return_value.status_code = 201
-            mock_requests_post.return_value.json.return_value = {"valid": True}
-            response = self.client.get('/products/1234567890123/warehouses', 
-                headers= headers
-            )
-            print(response.json)
-            self.assertEqual(response.status_code, 200)
-            self.assertGreater(len(response.json['warehouse_info']), 0)
-
+        service = GetById("123ABC", token="dummy-token")
+        result = service.execute()
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result['product_info']['product_name'], "Producto A")
+        self.assertEqual(result['warehouse_info']['warehouse_name'], "Bodega A")
+        
+        
