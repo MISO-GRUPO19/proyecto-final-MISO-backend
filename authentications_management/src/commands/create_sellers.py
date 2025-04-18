8478d701665700d297d1e58f76937a6fd5be69e5
Juan Camilo <j.villadap@uniandes.edu.co>
2025-04-17 21:17:05 -0500

    primera version de la historia de usuario lista

--- authentications_management/src/commands/create_sellers.py
+++ authentications_management/src/commands/create_sellers.py
@@ -1,10 +1,13 @@
 from .base_command import BaseCommand
 from ..errors.errors import *
-from ..models.sellers import Sellers
+from ..models.sellers import Sellers, GoalProduct, Goals
 from ..models.database import db_session
 from .create_users import CreateUsers
 from flask import jsonify
 import re
+import random
+from datetime import datetime
+import uuid
 
 
 ALLOWED_COUNTRIES = [
@@ -17,6 +20,32 @@ class CreateSellers(BaseCommand):
     def __init__(self, data):
         self.data = data
 
+    ''' HU11 Reporte de ventas vendedores '''
+    def create_fake_goal(self, seller: Sellers):
+        date = datetime(2025,random.randint(1,12),1)
+        goal = Goals(
+            seller_id=seller.id,
+            date=date
+        )
+        db_session.add(goal)
+        db_session.commit()
+        return goal
+
+    def create_fake_goal_products(self, goal_created: Goals):
+        quantity = random.randint(20, 100)
+        sales = round(quantity * round(random.uniform(10.0, 100.0),2))
+        goal_product = GoalProduct(
+            product_id=uuid.uuid4(),
+            quantity=quantity,
+            goal_id=goal_created.id,
+            date=goal_created.date,
+            sales=sales
+        )
+        db_session.add(goal_product)
+        db_session.commit()
+
+
+    ''' FIN HU11 REPORTE DE VENTAS VENDEDORES'''
     def execute(self):
         if (self.data['name'] == '' or self.data['country'] == '' or self.data['identification'] == '' or self.data['address'] == '' or self.data['telephone'] == '' or self.data['email'] == '' ):
             raise InvalidData
@@ -63,6 +92,11 @@ class CreateSellers(BaseCommand):
         if user_seller['message'] == 'Usuario enviado a la cola exitosamente':
             db_session.add(seller)
             db_session.commit()
+            '''INICIO MODIFICACIÓN REPORTE DE VENTAS VENDEDORES'''
+            goal = self.create_fake_goal(seller)
+            for i in range(random.randint(2, 5)):
+                self.create_fake_goal_products(goal)
+
             return {'message': 'Seller has been created successfully'}
     
     def check_identification(self, identification: str):
--- authentications_management/src/models/sellers.py
+++ authentications_management/src/models/sellers.py
@@ -1,11 +1,12 @@
 from marshmallow import Schema, fields
 from flask_sqlalchemy import SQLAlchemy
-from  sqlalchemy  import  Column, String, Integer, DateTime, CheckConstraint, Enum, Float
+from  sqlalchemy  import  Column, String, Integer, DateTime, CheckConstraint, Enum, Float, ForeignKey, UniqueConstraint
 from .model  import  Model
 from .database import base
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
 from enum import Enum
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
@@ -20,6 +21,8 @@ class Sellers(Model, base):
     telephone = Column(String, nullable=False)
     email = Column(String, nullable=False)
 
+    goals = relationship('Goals', back_populates='seller', cascade='all, delete-orphan')
+
     def __init__(self, name, identification, country, address, telephone, email):
         self.name = name
         self.identification = identification
@@ -27,4 +30,42 @@ class Sellers(Model, base):
         self.address = address
         self.telephone = telephone
         self.email = email
+
+class Goals(Model, base):
+    __tablename__ = "goals"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    seller_id = Column(UUID(as_uuid=True), ForeignKey('sellers.id'), nullable=False)
+    date = Column(DateTime, nullable=False)
+
+    __table_args__ = (
+        UniqueConstraint('id', 'seller_id', 'date', name='uq_goals_id_seller_date'),
+    )
+
+    seller = relationship('Sellers', back_populates='goals')
+
+    goals_product = relationship('GoalProduct', back_populates='goal', cascade='all, delete-orphan')
+
+    def __init__(self, seller_id, date):
+        self.seller_id = seller_id
+        self.date = date
+        
+
+class GoalProduct(Model, base):
+    __tablename__ = 'goal_product'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    product_id = Column(UUID(as_uuid=True), nullable=False)
+    quantity = Column(Integer, nullable=False, default=0)
+    goal_id = Column(UUID(as_uuid=True), ForeignKey('goals.id'),nullable=False)
+    date = Column(DateTime, nullable=False) 
+    goal = relationship('Goals', back_populates='goals_product')
+    sales = Column(Float, nullable=False, default=0)
+
+    def __init__(self, product_id, quantity, goal_id, date,sales):
+        self.product_id = product_id
+        self.quantity = quantity
+        self.goal_id = goal_id
+        self.date = date
+        self.sales = sales
         
