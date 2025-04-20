b526ef43c1b077692a21f0d62ae7d70070e46213
Carlos Tiguaque <c.tiguaque@uniandes.edu.co>
2025-04-20 17:57:02 -0500

    Se ajustan pruebas

--- authentications_management/src/models/database.py
+++ authentications_management/src/models/database.py
@@ -7,7 +7,12 @@ load_dotenv('.env.development')
 
 # Configuración de la conexión a la base de datos
 if os.getenv('FLASK_ENV') == 'testing':
-    urldb = 'sqlite:///:memory:'  
+    urldb = 'sqlite:///:memory:'
+    engine = create_engine(
+        urldb,
+        connect_args={"check_same_thread": False},
+        echo=False
+    )
 else:
     userdb = os.environ["DB_USER"]
     password = os.environ["DB_PASSWORD"]
@@ -15,16 +20,15 @@ else:
     dbname = os.environ["DB_NAME"]
     port_db = os.environ["DB_PORT"]
     urldb = f'postgresql://{userdb}:{password}@{host}:{port_db}/{dbname}'
-
-engine = create_engine(
-    urldb,
-    pool_size=25,
-    max_overflow=10,
-    pool_timeout=10,
-    pool_recycle=180,
-    pool_pre_ping=True,
-    echo=False
-)
+    engine = create_engine(
+        urldb,
+        pool_size=25,
+        max_overflow=10,
+        pool_timeout=10,
+        pool_recycle=180,
+        pool_pre_ping=True,
+        echo=False
+    )
 
 db_session = scoped_session(sessionmaker(
     autocommit=False,
