2d2f8fcdfe694610456b179166950d5c1ba13d50
Carlos Tiguaque <c.tiguaque@uniandes.edu.co>
2025-04-21 20:05:36 -0500

    Se crea endpoint para consultar orden por id

--- crear_productos.sh
+++ crear_productos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ENDPOINT="https://nginx-service-230506421700.us-central1.run.app/products"
-TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTc0NTE4NjA1MCwianRpIjoiOGM1MjY1ZGItNTJjOS00ODUzLTkyYWQtMTkzYjk3Y2I4YzZmIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImNlN2NkYzA1LTBhOGUtNDVjMS1hYmQzLTI3YWU2ZDRiZGM0ZiIsIm5iZiI6MTc0NTE4NjA1MCwiY3NyZiI6IjQ4YjcxOTA2LTUxOTQtNDE1ZC05OGE3LWUzZjM4Yjg2MjY2OCIsImV4cCI6MTc0NTE4Njk1MH0.9rW4VP9rjR8LoBumZwD2JqbWS1lwVcxW3Lpr9LL-nwI"
+ENDPOINT="http://127.0.0.1:8080/products"
+TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTc0NTI4MTQzMiwianRpIjoiNTVhOGM2NTUtMTQwMi00NzMyLWIxMTItMjExODZhNzNiYWE4IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImEzZjQ4YWE3LWY2OGUtNDkyMy04ZTQ3LTBkNDJlN2QwMjNlYiIsIm5iZiI6MTc0NTI4MTQzMiwiY3NyZiI6IjVjN2RmMzYzLTNlMzAtNGRiNS1iZDUzLTIzNzMxZjY2MTVjMCIsImV4cCI6MTc0NTI4MjMzMn0.e98ZUh62gnjhe2Gq1SVXejZ2EVoRgQAWsfo-BfsrGO8"
 
 HEADERS=(-H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json")
 
@@ -13,7 +13,7 @@ declare -a BODIES=(
   "category": "Snacks y Dulces",
   "weight": 0.15,
   "barcode": "7702011111111",
-  "provider_id": "c87bfe8e-18f0-4089-b713-f7f246ae9ac2",
+  "provider_id": "c9953c9a-846e-4542-8a87-bd38ee478177",
   "batch": "Batch001",
   "best_before": "2025-12-31T23:59:59",
   "quantity": 100
@@ -25,7 +25,7 @@ declare -a BODIES=(
   "category": "Snacks y Dulces",
   "weight": 0.03,
   "barcode": "7702011222222",
-  "provider_id": "c87bfe8e-18f0-4089-b713-f7f246ae9ac2",
+  "provider_id": "c9953c9a-846e-4542-8a87-bd38ee478177",
   "batch": "Batch002",
   "best_before": "2025-11-30T23:59:59",
   "quantity": 200
@@ -37,7 +37,7 @@ declare -a BODIES=(
   "category": "Snacks y Dulces",
   "weight": 0.05,
   "barcode": "7702011333333",
-  "provider_id": "c87bfe8e-18f0-4089-b713-f7f246ae9ac2",
+  "provider_id": "c9953c9a-846e-4542-8a87-bd38ee478177",
   "batch": "Batch003",
   "best_before": "2026-01-15T23:59:59",
   "quantity": 150
@@ -49,7 +49,7 @@ declare -a BODIES=(
   "category": "Panadería y Repostería",
   "weight": 0.2,
   "barcode": "7702011444444",
-  "provider_id": "c87bfe8e-18f0-4089-b713-f7f246ae9ac2",
+  "provider_id": "c9953c9a-846e-4542-8a87-bd38ee478177",
   "batch": "Batch004",
   "best_before": "2025-10-10T23:59:59",
   "quantity": 80
@@ -61,7 +61,7 @@ declare -a BODIES=(
   "category": "Snacks y Dulces",
   "weight": 0.02,
   "barcode": "7702011555555",
-  "provider_id": "c87bfe8e-18f0-4089-b713-f7f246ae9ac2",
+  "provider_id": "c9953c9a-846e-4542-8a87-bd38ee478177",
   "batch": "Batch005",
   "best_before": "2025-09-01T23:59:59",
   "quantity": 300
@@ -73,7 +73,7 @@ declare -a BODIES=(
   "category": "Lácteos y Huevos",
   "weight": 0.1,
   "barcode": "7702011666666",
-  "provider_id": "c87bfe8e-18f0-4089-b713-f7f246ae9ac2",
+  "provider_id": "c9953c9a-846e-4542-8a87-bd38ee478177",
   "batch": "Batch006",
   "best_before": "2025-12-01T23:59:59",
   "quantity": 250
@@ -85,7 +85,7 @@ declare -a BODIES=(
   "category": "Snacks y Dulces",
   "weight": 0.08,
   "barcode": "7702011777777",
-  "provider_id": "c87bfe8e-18f0-4089-b713-f7f246ae9ac2",
+  "provider_id": "c9953c9a-846e-4542-8a87-bd38ee478177",
   "batch": "Batch007",
   "best_before": "2025-08-20T23:59:59",
   "quantity": 120
@@ -97,7 +97,7 @@ declare -a BODIES=(
   "category": "Snacks y Dulces",
   "weight": 0.03,
   "barcode": "7702011888888",
-  "provider_id": "c87bfe8e-18f0-4089-b713-f7f246ae9ac2",
+  "provider_id": "c9953c9a-846e-4542-8a87-bd38ee478177",
   "batch": "Batch008",
   "best_before": "2026-01-01T23:59:59",
   "quantity": 180
@@ -109,7 +109,7 @@ declare -a BODIES=(
   "category": "Snacks y Dulces",
   "weight": 0.02,
   "barcode": "7702011999999",
-  "provider_id": "c87bfe8e-18f0-4089-b713-f7f246ae9ac2",
+  "provider_id": "c9953c9a-846e-4542-8a87-bd38ee478177",
   "batch": "Batch009",
   "best_before": "2025-07-15T23:59:59",
   "quantity": 220
@@ -121,7 +121,7 @@ declare -a BODIES=(
   "category": "Panadería y Repostería",
   "weight": 0.07,
   "barcode": "7702012000001",
-  "provider_id": "c87bfe8e-18f0-4089-b713-f7f246ae9ac2",
+  "provider_id": "c9953c9a-846e-4542-8a87-bd38ee478177",
   "batch": "Batch010",
   "best_before": "2025-09-10T23:59:59",
   "quantity": 140
