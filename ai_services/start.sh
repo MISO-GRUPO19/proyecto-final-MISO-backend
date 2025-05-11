06f0c4cb37e55c31ae258c7fa6993e1d05bcfe07
Carlos Tiguaque <c.tiguaque@uniandes.edu.co>
2025-05-11 17:54:07 -0500

    Se ajusta nombre servicio

--- ai_services/start.sh
+++ ai_services/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Agregar el directorio raíz al PYTHONPATH
-export PYTHONPATH=/ai-services/src
+export PYTHONPATH=/ai_services/src
 
 # Iniciar el servicio de productos en segundo plano
 flask --app main.py --debug run --host=0.0.0.0 --port=5006 &
