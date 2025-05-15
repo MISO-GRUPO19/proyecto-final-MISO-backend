e77ca645a7d91727f32ecdd5b51427177821e64e
Juan Camilo <j.villadap@uniandes.edu.co>
2025-05-14 23:28:53 -0500

    cooregida

--- ai_services/tests/test_create_routes.py
+++ ai_services/tests/test_create_routes.py
@@ -16,31 +16,21 @@ def mock_token():
 
 @patch("ai_services.src.commands.create_routes.datetime")
 def test_validate_date(mock_datetime, mock_data, mock_token):
-    # Mock today's date
-    mock_datetime.now.return_value = datetime(2025, 5, 16)
-
-    # Create the command and validate the date
+    # Setup mock
+    mock_datetime.now.return_value.date.return_value = date(2025, 5, 16)
+    
     command = CreatRoute(mock_data, mock_token)
-    command.validate_date(mock_data["date"])  # Should not raise an exception
+    command.validate_date("17-05-2025")  # Should not raise
 
 def test_invalid_date_format(mock_data, mock_token):
-    # Provide an invalid date format
-    mock_data["date"] = "17/05/2025"  # Wrong format
-
-    # Create the command and validate the date
     command = CreatRoute(mock_data, mock_token)
     with pytest.raises(InvalidDate):
-        command.validate_date(mock_data["date"])
+        command.validate_date("17/05/2025")
 
 @patch("ai_services.src.commands.create_routes.datetime")
 def test_past_date(mock_datetime, mock_data, mock_token):
-    # Provide a past date
-    mock_data["date"] = "15-05-2025"
-
-    # Mock today's date
-    mock_datetime.now.return_value = datetime(2025, 5, 16)
-
-    # Create the command and validate the date
+    mock_datetime.now.return_value.date.return_value = date(2025, 5, 16)
+    
     command = CreatRoute(mock_data, mock_token)
     with pytest.raises(InvalidDate):
-        command.validate_date(mock_data["date"])
+        command.validate_date("15-05-2025")
